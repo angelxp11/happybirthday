12082918ebf664a80947f964ae3155f8637a982c
Jose Angel Bermúdez Choperena <jocheangel728@gmail.com>
2024-07-22 13:28:05 -0500

    subir el proyecto a react

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          va a ser la prueba de cumpleaños
         </p>
         <a
           className="App-link"
